31946f768ce26fd4393b9760f16959fab0d45b64
Mauro <mauro.mascheroni.97@hotmail.com>
2021-09-02 19:40:31 -0300

    Inicializamos repositorio del curso de JS

---
+++ figuras.js
@@ -0,0 +1,33 @@
+// Cuadrado
+
+const ladoCuadrado = 5;
+
+const perimetroCuadrado = ladoCuadrado * 4;
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+
+// Triángulos
+
+const ladoTrianguloUno = 6;
+
+const ladoTranguloDos = 6;
+
+const baseTriangulo = 4;
+
+const perimetroTriangulo = ladoTrianguloUno + ladoTranguloDos + baseTriangulo;
+
+const alturaTriangulo = 2;
+
+const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+
+// Circulos
+
+const radio = 6;
+
+const diametro = radio * 2;
+
+const PI = Math.PI;
+
+const perimetroCirculo = diametro * PI;
+
+const areaCirculo = radio * radio * PI;
